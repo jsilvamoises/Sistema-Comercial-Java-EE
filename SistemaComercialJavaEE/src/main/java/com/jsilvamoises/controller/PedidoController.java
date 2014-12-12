266dc096a82c99e20bb8af0f104cc0ecc77a93c1
MOISES <MOISES@MOISES-PC>
2014-12-11 22:53:41 -0200

    Criação Menu

---
+++ SistemaComercialJavaEE/src/main/java/com/jsilvamoises/controller/PedidoController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jsilvamoises.controller;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+/**
+ *
+ * @author MOISES
+ */
+@ManagedBean
+@RequestScoped
+public class PedidoController {
+    private List<Integer> lista;
+    public PedidoController(){
+        lista = new ArrayList<>();
+        for(int i= 0;i<50;i++){
+            lista.add(i);
+        }
+        
+    }
+    
+    public List<Integer> getPedidos(){
+        return lista;
+    }
+    
+}
